af286d262bfd768114af6d542484c40b87cc10ed
M Davies <wdavies@opentext.com>
2022-11-17 14:31:21 +0000

    Fixing small issues

---
+++ Shared/CsvGeneratorModel.cs
@@ -0,0 +1,33 @@
+﻿using Bogus.DataSets;
+
+namespace DevTools.Shared;
+
+public enum DataType
+{
+    Guid,
+    LoremWord
+}
+
+public sealed class CsvGeneratorModel
+{
+    public CsvGeneratorModel(string name, DataType type)
+    {
+        Name = name;
+        Type = type;
+    }
+        
+    public string Name { get; set; }
+    public DataType Type { get; set; }
+
+    public string GetData()
+    {
+        var lorem = new Lorem();
+            
+        return Type switch 
+        {
+            DataType.Guid => Guid.NewGuid().ToString(),
+            DataType.LoremWord => lorem.Word(),
+            _ => string.Empty
+        };
+    }
+}
