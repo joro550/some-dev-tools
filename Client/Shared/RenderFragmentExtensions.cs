b6e2c1edda1d93c4f968604c837a97a5ea8a391e
joro550 <markdavies550@gmail.com>
2022-11-22 22:05:12 +0000

    Fixing some styling stuff and making stuff easier

---
+++ Client/Shared/RenderFragmentExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+namespace DevTools.Client.Shared;
+
+public static class RenderFragmentExtensions
+{
+    public static void Default(this RenderTreeBuilder builder)
+    {
+        builder.AddContent(0, "<div>Content was Null</div>");
+    }
+}
